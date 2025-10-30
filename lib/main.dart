0f9e34758127921b0838460ebb4e68c78ea38cee
MuhammadFarrelFaiz <farrelfaizrabbani@gmail.com>
2025-10-30 18:47:26 +0700

    Add movie carousel and list components with sample data and images

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'components/movie_list_section.dart';
+import 'components/movie_carousel.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Duplicate Movies App',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
       home: const MyHomePage(title: 'Now Playing'),
     );
   }
@@ -30,6 +29,52 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<Map<String, String>> carouselMovies = [
+    {
+      'image': 'assets/images/tlw.jpg',
+      'title': 'The Long Walk',
+      'releaseYear': '2025',
+      'genre': 'Sci-Fi',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      'image': 'assets/images/tin-soldier.jpg',
+      'title': 'Tin Soldier',
+      'releaseYear': '2025',
+      'genre': 'Action',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      'image': 'assets/images/nobody-2.jpg',
+      'title': 'Nobody 2',
+      'releaseYear': '2025',
+      'genre': 'Action',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+  ];
+
+  final List<Map<String, String>> trendingMovies = [
+    {'image': 'assets/images/mi-1.jpg'},
+    {'image': 'assets/images/star-wars-1.jpg'},
+    {'image': 'assets/images/star-wars-2.jpg'},
+    {'image': 'assets/images/star-wars-3.jpg'},
+    {'image': 'assets/images/transformers-1.jpeg'},
+  ];
+  final List<Map<String, String>> popularMovies = [
+    {'image': 'assets/images/transformers-2.jpg'},
+    {'image': 'assets/images/casino-royale.jpg'},
+    {'image': 'assets/images/skyfall.jpg'},
+    {'image': 'assets/images/avatar.jpg'},
+    {'image': 'assets/images/fast-x.jpg'},
+  ];
+  final List<Map<String, String>> topRatedMovies = [
+    {'image': 'assets/images/godfather-1.jpg'},
+    {'image': 'assets/images/godfather-2.jpg'},
+    {'image': 'assets/images/se7en.jpg'},
+    {'image': 'assets/images/spr.jpg'},
+    {'image': 'assets/images/coco.jpg'},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,22 +94,17 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Column(
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      "Trending",
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+            // Carousel
+            MovieCarousel(movies: carouselMovies),
+            const SizedBox(height: 50),
+
+            // Movie Lists
+            MovieListSection(title: "Trending", movies: trendingMovies),
+            const SizedBox(height: 20),
+            MovieListSection(title: "Popular", movies: popularMovies),
+            const SizedBox(height: 20),
+            MovieListSection(title: "Top Rated", movies: topRatedMovies),
+            const SizedBox(height: 50),
           ],
         ),
       ),
